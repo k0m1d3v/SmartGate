7b03da2b18e8ca5d0488e970ad168109818e8038
Nicola Amicone <amiconen2@gmail.com>
2024-10-02 18:18:46 +0200

    Almost there

--- client/client.py
+++ client/client.py
@@ -1,9 +1,63 @@
 import cv2
 import requests
 import time
+import threading
+import RPi.GPIO as GPIO
+
+# Imposta il pin di controllo del servo
+SERVO_PIN = 17  # Cambia il pin in base al collegamento del tuo servo
+
+
+# Configurazione di base per il servo
+def setup_servo():
+    GPIO.setmode(GPIO.BCM)  # Imposta il modo BCM per i pin
+    GPIO.setup(SERVO_PIN, GPIO.OUT)
+
+    # Crea un oggetto PWM sul pin del servo a 50Hz (frequenza standard per i servi)
+    pwm = GPIO.PWM(SERVO_PIN, 50)
+    pwm.start(0)  # Inizializza il servo con un duty cycle di 0
+    return pwm
+
+
+# Funzione per aprire il cancello (ruotare il servo di almeno 720 gradi)
+def apri_cancello(pwm):
+    print("Apertura del cancello...")
+    pwm.ChangeDutyCycle(7)  # Modifica il duty cycle per ruotare il servo in una direzione
+    time.sleep(4)  # Attendere il tempo necessario per ruotare di 720 gradi
+    pwm.ChangeDutyCycle(0)  # Ferma il servo
+    print("Cancello aperto!")
+
+
+# Funzione per chiudere il cancello (ruotare il servo nella direzione opposta per 720 gradi)
+def chiudi_cancello(pwm):
+    print("Chiusura del cancello...")
+    pwm.ChangeDutyCycle(5)  # Modifica il duty cycle per ruotare il servo nella direzione opposta
+    time.sleep(4)  # Attendere il tempo necessario per ruotare di 720 gradi
+    pwm.ChangeDutyCycle(0)  # Ferma il servo
+    print("Cancello chiuso!")
+
+
+# Funzione per pulire i pin GPIO alla fine
+def cleanup_servo():
+    GPIO.cleanup()
 
 # Indirizzo del server Flask
-SERVER_URL = "http://192.168.187.113:5000/process_image"
+SERVER_URL = "http://192.168.1.19:5000/process_image"
+Active = False
+def start_timer(n):
+    global Active
+    Active = True
+    def timer():
+        global Active
+        print(f"Cancello in apertura per {n} secondi")
+        time.sleep(n)
+        print("Cancello in chiusura!")
+        time.sleep(5)
+        Active = False
+
+    timer_thread = threading.Thread(target=timer)
+    timer_thread.daemon = True
+    timer_thread.start()
 
 
 def capture_and_send_image():
@@ -49,6 +103,8 @@ def capture_and_send_image():
                             else:
                                 print(f"Targa sconosciuta: {plate_text}")
                             print(f"Volti rilevati: {face_names}")
+                            if face_names != "Sconosciuto" and face_names != "" and not Active or amroÈaraboEdÈEgiziano:
+                                start_timer(15)
                         else:
                             print("Formato della risposta JSON inatteso:", response_data)
 
